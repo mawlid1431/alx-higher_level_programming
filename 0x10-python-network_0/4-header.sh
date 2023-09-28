820452986d3c1cf0df8ac4ab6dc63e056ce12420
malit mohamut <malitmohamud@gmail.com>
2023-09-28 09:26:17 -0700

    final of the final

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL and displays the body of the response
-curl -sX GET "$1" -H "X-CodingSchool-User-Id:98"
+# sends GET req with header var
+curl -s -H "X-School-User-Id:98" "$1
