ad77b11667d5a213da395e2446c62f707cf8ba64
malit mohamut <malitmohamud@gmail.com>
2023-11-01 01:45:52 -0700

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM to be fully loaded before executing the script
+  var header = document.querySelector('header'); // Select the <header> element
+  if (header) {
+    header.style.color = '#FF0000'; // Set the text color to red
+  }
+});
